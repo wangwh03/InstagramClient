34222693dcc686db5ab6e9fc7d52393fe96985db
Weihua Wang <wewang@groupon.com>
2015-10-25 23:33:32 -0500

    fetch and display photos from Instagram

---
+++ app/src/main/java/com/wewang/instagramclient/InstagramPhotosAdapter.java
@@ -0,0 +1,40 @@
+package com.wewang.instagramclient;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+/**
+ * Created by wewang on 10/25/15.
+ */
+public class InstagramPhotosAdapter extends ArrayAdapter<InstagramPhoto> {
+    public InstagramPhotosAdapter(Context context, List<InstagramPhoto> objects) {
+        super(context, android.R.layout.simple_list_item_1, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        InstagramPhoto photo = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
+        }
+
+        TextView tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
+        ImageView ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+
+        tvCaption.setText(photo.caption);
+        ivPhoto.setImageResource(0);
+
+        Picasso.with(getContext()).load(photo.imageUrl).into(ivPhoto);
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/wewang/instagramclient/PhotosActivity.java
@@ -0,0 +1,94 @@
+package com.wewang.instagramclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class PhotosActivity extends AppCompatActivity {
+
+    public static final String CLIENT_ID = "96b7dfac3e744f31ab0791fe409619d5";
+    private ArrayList<InstagramPhoto> photos;
+    private InstagramPhotosAdapter aPhotos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photos);
+        photos = new ArrayList<>();
+        aPhotos = new InstagramPhotosAdapter(this, photos);
+        ListView lvPhotos = (ListView) findViewById(R.id.lvPhotos);
+        lvPhotos.setAdapter(aPhotos);
+        fetchPopularPhotos();
+    }
+
+    public void fetchPopularPhotos() {
+        String url = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray photosJson = null;
+                try {
+                    photosJson = response.getJSONArray("data");
+                    for (int i = 0; i < photosJson.length(); i++) {
+                        JSONObject photoJSON = photosJson.getJSONObject(i);
+                        InstagramPhoto photo = new InstagramPhoto();
+                        photo.username = photoJSON.getJSONObject("user").getString("username");
+                        photo.caption = photoJSON.getJSONObject("caption").getString("text");
+                        photo.imageUrl = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
+                        photo.imageHeight = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
+                        photo.likesCount = photoJSON.getJSONObject("likes").getInt("count");
+
+                        photos.add(photo);
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                aPhotos.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_photos, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
